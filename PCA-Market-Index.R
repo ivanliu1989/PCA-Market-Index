cbec3d4662413a435599483a77eceac5147e532e
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 09:58:03 +1000

    pca

--- PCA-Market-Index.R
+++ PCA-Market-Index.R
@@ -16,3 +16,11 @@ correlations <- as.numeric(cor.matrix)
 require(ggplot2)
 p <- ggplot(data.frame(Correlation=correlations), aes(x=Correlation, fill=1))
 p + geom_density() + theme(legend.position='none') + ggsave('cor.png')
+
+# pca
+pca <- princomp(date.stock.matrix[,2:ncol(date.stock.matrix)])
+pca
+principal.component <- pca$loadings[,1]
+loadings <- as.numeric(principal.component)
+p2 <- ggplot(data.frame(Loading=loadings), aes(x=Loading,fill=1))
+p2+geom_density()+theme(legend.position='none')+ggsave('pca1.png')
