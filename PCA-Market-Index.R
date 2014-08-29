072e2d1570471338286ced9a10a804475e3f2573
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-29 10:06:08 +1000

    comparison

--- PCA-Market-Index.R
+++ PCA-Market-Index.R
@@ -24,3 +24,17 @@ principal.component <- pca$loadings[,1]
 loadings <- as.numeric(principal.component)
 p2 <- ggplot(data.frame(Loading=loadings), aes(x=Loading,fill=1))
 p2+geom_density()+theme(legend.position='none')+ggsave('pca1.png')
+
+# predict
+market.index <- predict(pca)[,1]
+dji.prices <- read.csv('data/DJI.csv')
+dji.prices <- transform(dji.prices,Date=ymd(Date))
+dji.prices <- subset(dji.prices,Date>ymd('2001-12-31'))
+dji.prices <- subset(dji.prices, Date != ymd('2002-02-01'))
+dji <- with(dji.prices, rev(Close))
+dates <- with(dji.prices, rev(Date))
+
+# plot
+comparison <- data.frame(Date=dates,MarketIndex=market.index, DJI=dji)
+p3 <- ggplot(comparison, aes(x=MarketIndex,y=DJI))
+p3 + geom_point()+geom_smooth(method='lm',se=F)+ggsave('comparison.png')
